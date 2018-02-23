9a63994125cca7bd837d59dfa5e09f9780d4e1d3
adershmanoj <adershmanoj@gmail.com>
2018-02-23 12:33:59 +0530

    Formatted console output

--- api/tagger.js
+++ api/tagger.js
@@ -9,7 +9,10 @@ export default function tag(reviewData){
 			sentences = obj.text.split('.'); //split review text into sentence array
 			sentences.forEach(function(sentence){
 				var analysis = emotional.get(sentence); //subjectivity and polarity analysis
-				console.log(sentence + ':\t' + analysis);
+				console.log(sentence + ':\t');
+				console.log(analysis);
+				console.log('------------- Tagger output -----------------');
+				console.log(sentence);
 				words = sentence.split(' '); //word array
 				tags = new Tag(words)
 				.initial() // initial dictionary and pattern based tagging 
@@ -17,7 +20,6 @@ export default function tag(reviewData){
 				.tags;
 				
 				let regex = /NN/; // regex to match NN, NNP, NNPS
-				console.log('------------- Tagger output -----------------')
 				for(i=0;i<tags.length;i++){
 					if(regex.test(tags[i])){ // Noun
 						console.log(words[i]);		
