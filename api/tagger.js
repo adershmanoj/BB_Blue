2d6b61c1c1ce509acc9c758148206e6904d92aff
adershmanoj <adershmanoj@gmail.com>
2018-02-23 12:23:06 +0530

    Formatted console output

--- api/tagger.js
+++ api/tagger.js
@@ -3,14 +3,13 @@ const Tag = require("en-pos").Tag;
 var emotional = require("emotional");
 
 export default function tag(reviewData){
-	console.log('-------------Crawler output -----------------');
-	console.log(reviewData);
+	console.log('------------- Crawler and sentiment analysis output -----------------');
 	reviewData.reviews.forEach(function(obj){
 		emotional.load(function () {
 			sentences = obj.text.split('.'); //split review text into sentence array
 			sentences.forEach(function(sentence){
 				var analysis = emotional.get(sentence); //subjectivity and polarity analysis
-				
+				console.log(sentence + ':\t' + analysis);
 				words = sentence.split(' '); //word array
 				tags = new Tag(words)
 				.initial() // initial dictionary and pattern based tagging 
@@ -18,6 +17,7 @@ export default function tag(reviewData){
 				.tags;
 				
 				let regex = /NN/; // regex to match NN, NNP, NNPS
+				console.log('------------- Tagger output -----------------')
 				for(i=0;i<tags.length;i++){
 					if(regex.test(tags[i])){ // Noun
 						console.log(words[i]);		
