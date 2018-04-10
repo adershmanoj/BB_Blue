c2f833f017f5551abf0ae13672394aef5dbd02cf
Bharath Mohan <bharathmohan.m@gmail.com>
2018-04-10 22:11:15 +0530

    UI Parts

--- api/tagger.js
+++ api/tagger.js
@@ -2,30 +2,37 @@ import {Meteor} from 'meteor/meteor';
 const Tag = require("en-pos").Tag;
 var emotional = require("emotional");
 var nounList = []; //store nouns and count
+let senRegEx = /w+/
+let regex = /^NN/; // regex to match NN, NNP, NNPS
 
+// let featureList = [];
 export default function tag(reviewData){
 	console.log('------------- Crawler and sentiment analysis output -----------------');
 	reviewData.reviews.forEach(function(obj){
 		emotional.load(function () {
+			console.log('--------------');
 			sentences = obj.text.split('.'); //split review text into sentence array
 			sentences.forEach(function(sentence){
-				var analysis = emotional.get(sentence); //subjectivity and polarity analysis
-				console.log(sentence);
-				console.log('subjectivity: ' + analysis.subjectivity);
-				console.log('------------- Tagger output -----------------');
-				if(analysis.subjectivity > 0.3){
-					console.log("success");
-//					words = sentence.split(' '); //word array
-//					tags = new Tag(words)
-//					.initial() // initial dictionary and pattern based tagging 
-//					.smooth() // further context based smoothing 
-//					.tags;
-//					let regex = /NN/; // regex to match NN, NNP, NNPS
-//					for(i=0;i<tags.length;i++){
-//						if(regex.test(tags[i])){ // Noun
-//							console.log(words[i]);		
-//						}
-//					}
+				if(senRegEx.test(sentence)){
+					console.log("sentence: " + sentence);
+					var analysis = emotional.get(sentence); //subjectivity and polarity analysis
+					if(analysis.subjectivity > 0.3){
+						console.log('subjectivity: ' + analysis.subjectivity);
+						console.log('polarity: ' + analysis.polarity);
+						words = sentence.split(' '); //word array
+						tags = new Tag(words)
+						.initial() // initial dictionary and pattern based tagging 
+						.smooth() // further context based smoothing 
+						.tags;
+						for(i=0;i<tags.length;i++){
+							if(regex.test(tags[i])){ // Noun
+								console.log("Tagger: " + words[i]);	
+								// if(!featureList.includes(words[i])) {
+								// 	featureList.push()
+								// }
+							}
+						}
+					}
 				}
 			});
 		});		
